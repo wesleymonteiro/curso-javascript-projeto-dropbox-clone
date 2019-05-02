3858f23a02babe607d9a3f6728da7fd1ecba5518
Wesley Monteiro <wesley.monteiro@cadena.com.br>
2019-05-02 16:20:42 -0300

    updates modal to show progress and to reset when finished

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,6 +3,9 @@ class DropBoxController {
     this.btnSendFileEl = document.querySelector('#btn-send-file')
     this.inputFilesEl = document.querySelector('#files')
     this.snackModalEl = document.querySelector('#react-snackbar-root')
+    this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
+    this.fileNameEl = this.snackModalEl.querySelector('.filename')
+    this.timeLeftEl = this.snackModalEl.querySelector('.timeleft')
     this.initEvents()
   }
 
@@ -14,8 +17,14 @@ class DropBoxController {
     this.inputFilesEl.addEventListener('change', event => {
       this.uploadTask(event.target.files)
 
-      this.snackModalEl.style.display = 'block'
+      this.showModal()
+
+      this.inputFilesEl.value = ''
     })
+  }
+
+  showModal(show = true) {
+    this.snackModalEl.style.display = show ? 'block' : 'none'
   }
 
   uploadTask(files) {
@@ -33,19 +42,53 @@ class DropBoxController {
           } catch (e) {
             reject(e)
           }
+
+          this.showModal(false)
         }
         ajax.onerror = error=>{
           reject(error)
+
+          this.showModal(false)
+        }
+        ajax.upload.onprogress = event=>{
+          this.uploadProgress(event, file)
         }
 
         let formData = new FormData()
 
         formData.append('input-file', file)
 
+        this.startTime = Date.now()
         ajax.send(formData)
       }))
     })
 
     return Promise.all(promises);
   }
+
+  uploadProgress(event, file) {
+    let timeSpent = Date.now() - this.startTime
+    let percent = parseInt((event.loaded / event.total) * 100)
+    let timeLeft = ((100 - percent) * timeSpent) / percent
+    this.progressBarEl.style.width = `${percent}%`
+
+    this.fileNameEl.innerHTML = file.name
+    this.timeLeftEl.innerHTML = this.formatTime(timeLeft)
+  }
+
+  formatTime(dur) {
+    let seconds = parseInt((dur / 1000) % 60)
+    let minutes = parseInt((dur / (1000 * 60)) % 60)
+    let hours = parseInt((dur / (1000 * 60 * 60)) % 24)
+
+    if (hours > 0)
+      return `${hours} horas, ${minutes} minutos e ${seconds} segundos`
+    if (minutes > 0)
+      return `${minutes} minutos e ${seconds} segundos`
+    if (seconds > 0)
+      return `${seconds} segundos`
+
+    return ''
+  }
+
 }
